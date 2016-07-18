6c5d4d555cdb892dccf537c6a61bc9a79febccd1
Mads <madslind@gmail.com>
2016-07-18 22:02:23 +0200

    Initial commit

---
+++ project/routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { user: req.user });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+  console.log("Hit register POST");
+  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+    console.log("register callback, " + err + ", acc: " + account);
+    console.log("req.body.username: " + req.body.username);
+    if (err) {
+
+      return res.render('register', { account : account });
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      res.redirect('/');
+    });
+  });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/ping', function(req, res){
+  console.log(req.query.test);
+  res.status(200).send("pong!");
+});
+
+
+module.exports = router;
